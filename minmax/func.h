fc5e15a395dc7459eac03c52e33299c4539c1ba1
Minchao Zhu <zhuminchao@aliyun.com>
2019-06-28 11:22:42 +0800

    Add a widget project minmax: to find the max and min number in a number input list.

---
+++ minmax/func.h
@@ -0,0 +1,14 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <vector>
+using namespace std;
+class func
+{
+public:
+    func();
+    int min(vector<int>);
+    int max(vector<int>);
+};
+
+#endif // FUNC_H
